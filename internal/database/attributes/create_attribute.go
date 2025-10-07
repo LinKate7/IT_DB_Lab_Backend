c4c4bd92aa3dfe9541a0eadfb986d6a0999162f2
Kateryna Lahoda <kateryna.lahoda@softengi.com>
2025-10-07 22:44:01 +0300

    Second commit

---
+++ internal/database/attributes/create_attribute.go
@@ -0,0 +1,20 @@
+package attributes
+
+import "fmt"
+
+func CreateAttribute(attr_type string, attr_name string) (Attribute, error) {
+	switch attr_type {
+	case "char":
+		return NewCharAttribute(attr_name), nil
+	case "integer":
+		return NewIntegerAttribute(attr_name), nil
+	case "string":
+		return NewStringAttribute(attr_name), nil
+	case "real":
+		return NewRealAttribute(attr_name), nil
+	case "color":
+		return NewColorAttribute(attr_name), nil
+	default:
+		return nil, fmt.Errorf("type %v not found", attr_type)
+	}
+}
